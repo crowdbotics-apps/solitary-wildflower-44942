99d8daab8125aabdf30303e60d7cb1d65f1df2e7
Crowdbotics <team@crowdbotics.com>
2023-12-11 01:14:40 +0000

    Initial commit

---
+++ backend/solitary_wildflower_44942/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for solitary_wildflower_44942 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "solitary_wildflower_44942.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-wildflower-44942",
+  url: "https://solitary-wildflower-44942.botics.co",
+  api: "https://solitary-wildflower-44942.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
